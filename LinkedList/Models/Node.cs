36540c3f3fe7b15c600ae0f8d1638ce8c647c9cb
rox <therox116@gmail.com>
2021-03-18 00:14:57 -0500

    initial commit

---
+++ LinkedList/Models/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructuresWeb.LinkedList.Models
+{
+    public class Node
+    {
+        public  Node next
+        {
+            get => next;
+            set=>value=next;
+        }
+    
+        public int data { get; set; }
+
+        public Node(int data) => this.data = data;
+    }
+}
